cf4ba6fac78cb4fbe2b0a065b6d94125dfca14c1
= <=>
2025-12-22 23:06:41 +0300

    mvp full benchmark

---
+++ tests/performance/benchmark.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+# Written in [Amber](https://amber-lang.com/)
+# version: 0.5.1-alpha
+# We cannot import `bash_version` from `env.ab` because it imports `text.ab` making a circular dependency.
+# This is a workaround to avoid that issue and the import system should be improved in the future.
+file_append__40_v0() {
+    local path=$1
+    local content=$2
+    command_0="$(echo "${content}" >> "${path}")"
+    __status=$?
+    if [ "${__status}" != 0 ]; then
+        ret_file_append40_v0=''
+        return "${__status}"
+    fi
+    ret_file_append40_v0="${command_0}"
+    return 0
+}
+
+usecases_3=("webhook-direct" "webhook-tgin" "longpull-tgin" "longpull-direct")
+rps_values_4=(100 200 300 400 500)
+make clean
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
+make build
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
+for rps_5 in "${rps_values_4[@]}"; do
+    for mode_6 in "${usecases_3[@]}"; do
+        command_4="$(make ${mode_6} RPS=${rps_5})"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        output_7="${command_4}"
+        command_5="$(echo "${output_7}" | grep "Requests Sent:" | tr -s ' ' | xargs | cut -d ' ' -f 3)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        sent_8="${command_5}"
+        command_6="$(echo "${output_7}" | grep "Responses Recv:" | tr -s ' ' | xargs | cut -d ' ' -f 3)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        recv_9="${command_6}"
+        command_7="$(echo "${output_7}" | grep "Errors (Net):" | tr -s ' ' | xargs | cut -d ' ' -f 3)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        errors_10="${command_7}"
+        command_8="$(echo "${output_7}" | grep "Loss Rate:" | tr -s ' ' | xargs | cut -d ' ' -f 3 | tr -d '%')"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        loss_11="${command_8}"
+        command_9="$(echo "${output_7}" | grep "Min:" | tr -s ' ' | xargs | cut -d ' ' -f 2)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        min_12="${command_9}"
+        command_10="$(echo "${output_7}" | grep "Mean:" | tr -s ' ' | xargs | cut -d ' ' -f 2)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        mean_13="${command_10}"
+        command_11="$(echo "${output_7}" | grep "p50:" | tr -s ' ' | xargs | cut -d ' ' -f 2)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        p50_14="${command_11}"
+        command_12="$(echo "${output_7}" | grep "p95:" | tr -s ' ' | xargs | cut -d ' ' -f 2)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        p95_15="${command_12}"
+        command_13="$(echo "${output_7}" | grep "p99:" | tr -s ' ' | xargs | cut -d ' ' -f 2)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        p99_16="${command_13}"
+        command_14="$(echo "${output_7}" | grep "Max:" | tr -s ' ' | xargs | cut -d ' ' -f 2)"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        max_17="${command_14}"
+        echo "${mode_6}"
+        echo "RPS=${rps_5} -> Sent: ${sent_8}, Resv: ${recv_9}, Errors: ${errors_10}, Loss: ${loss_11}%,  Min: ${min_12}ms, Mean: ${mean_13}ms, P50: ${p50_14}ms, P95: ${p95_15}ms, P99: ${p99_16}ms, Max: ${max_17}ms"
+        file_append__40_v0 "results.csv" "${mode_6},${rps_5},${sent_8},${recv_9},${errors_10},${loss_11},${min_12},${mean_13},${p50_14},${p95_15},${p99_16},${max_17}"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+        make clean
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            exit "${__status}"
+        fi
+    done
+done
